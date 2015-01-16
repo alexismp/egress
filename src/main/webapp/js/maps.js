3a159b7af9c7a04fd46ee16827b876edf1be2dfa
Alexis MP <alexis.mp@gmail.com>
2015-01-16 17:30:48 +0100

    * Implemented Google login via popup (with backend security rules) * removed managedvm flag * change map style * added legend with logged in user * added InfoWindow with oner of station * still need to work on authWithOAuthRedirect (async issue)
    {
        "rules": {
          ".read": "auth !== null", // read via geofire
          ".write": "auth !== null",
        
            "_geofire" :{
              ".read": "auth !== null",
              ".write": "auth !== null",  // all writes via geofire  
    
              
              // Index each location's geohash for faster querying
              ".indexOn": ["g"]
            }
        }
    }

--- src/main/webapp/js/maps.js
+++ src/main/webapp/js/maps.js
@@ -3,53 +3,79 @@
 /*****************/
 /* Initializes Google Maps */
 function initializeMap() {
-  // Get the location as a Google Maps latitude-longitude object
-  var loc = new google.maps.LatLng(center_stations[0], center_stations[1]);
+    // Create an array of styles.
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
 
-  // Create the Google Map
-  map = new google.maps.Map(document.getElementById("map-canvas"), {
-    center: loc,
-    zoom: 11,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
-  });
+    // Create a new StyledMapType object, passing it the array of styles,
+    // as well as the name to be displayed on the map type control.
+    var styledMap = new google.maps.StyledMapType(styles,
+            {name: "Styled Map"});
 
-  // Create a draggable circle centered on the map
-  var circle = new google.maps.Circle({
-    strokeColor: "#6D3099",
-    strokeOpacity: 0.7,
-    strokeWeight: 1,
-    fillColor: "#B650FF",
-    fillOpacity: 0.35,
-    map: map,
-    center: loc,
-    radius: ((radiusInKm_stations) * 1000),
-    draggable: true
-  });
+    // Get the location as a Google Maps latitude-longitude object
+    var loc = new google.maps.LatLng(center_stations[0], center_stations[1]);
 
-  //Update the query's criteria every time the circle is dragged
-  var updateCriteria = _.debounce(function() {
-    var latLng = circle.getCenter();
-    geoQuery_stations.updateCriteria({
-      center: [latLng.lat(), latLng.lng()],
-      radius: radiusInKm_stations
+    // Create the Google Map
+    map = new google.maps.Map(document.getElementById("map-canvas"), {
+        center: loc,
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
     });
-  }, 10);
-  google.maps.event.addListener(circle, "drag", updateCriteria);
+
+    // set the map style to active
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    map.controls[google.maps.ControlPosition.TOP].push(
+            document.getElementById('userlegend'));
+
+    // Create a draggable circle centered on the map
+    var circle = new google.maps.Circle({
+        strokeColor: "#6D3099",
+        strokeOpacity: 0.7,
+        strokeWeight: 1,
+        fillColor: "#a3c2ee",
+        fillOpacity: 0.35,
+        map: map,
+        center: loc,
+        radius: ((radiusInKm_stations) * 1000),
+        draggable: true
+    });
+
+    //Update the query's criteria every time the circle is dragged
+    var updateCriteria = _.debounce(function () {
+        var latLng = circle.getCenter();
+        geoQuery_stations.updateCriteria({
+            center: [latLng.lat(), latLng.lng()],
+            radius: radiusInKm_stations
+        });
+    }, 10);
+    google.maps.event.addListener(circle, "drag", updateCriteria);
 }
 
 function createStationMarker(station) {
-  var color;
-  if ( station.owner !== "" ) {
-    color = "888888";
-  } else {
-    color = "50B1FF" ;
-  }
-  var marker = new google.maps.Marker({
-    icon: "https://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=train|bbT|" + encodeURI(station.NOM) + "|" + color + "|555",
-    position: new google.maps.LatLng(station.LATITUDE, station.LONGITUDE),
-    optimized: true,
-    map: map
-  });
+    var bgcolor, fgcolor;
+    if (station.owner !== "") {
+        bgcolor = "888888";
+        fgcolor = "EEE";
+    } else {
+        bgcolor = "50B1FF";
+        fgcolor = "222";
+    }
+    // have fun with https://developers.google.com/chart/image/docs/gallery/dynamic_icons
+    var marker = new google.maps.Marker({
+        icon: "https://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=flag|bbT|" + encodeURI(station.NOM) + "|" + bgcolor + "|" + fgcolor,
+        position: new google.maps.LatLng(station.LATITUDE, station.LONGITUDE),
+        optimized: true,
+        map: map
+    });
 
-  return marker;
+    var infowindow = new google.maps.InfoWindow({
+        content: '<div style="overflow:hidden">'+station.owner+'</div>'
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+
+    return marker;
 }
--- src/main/webapp/js/trainEgress.js
+++ src/main/webapp/js/trainEgress.js
@@ -3,7 +3,7 @@ var map;
 
 // Set the center as Firebase HQ
 var locations = {
-  "Grenoble": [45.1841656, 5.7155425]
+    "Grenoble": [45.1841656, 5.7155425]
 };
 var center_stations = locations["Grenoble"];
 
@@ -12,26 +12,52 @@ var radiusInKm_stations = 8;
 
 // Get a reference to the Firebase public transit open data set
 // Create a new GeoFire instance, pulling data from the public transit data
-var trainStationsFirebaseRef = new Firebase("https://shining-inferno-9452.firebaseio.com/")
+var trainStationsFirebaseRef = new Firebase("https://shining-inferno-9452.firebaseio.com/");
 var geoFire_stations = new GeoFire(trainStationsFirebaseRef.child("_geofire"));
 
 // Create a callback to handle the result of the authentication
 function authHandler(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
+    if (error) {
+        console.log("Login Failed!", error);
+    } else {
+        console.log("Authenticated successfully with payload:", authData);
+        console.log("Name: ", authData.google.displayName);
+        updateUserUI(authData.google.displayName);
+    }
+}
+
+function updateUserUI(name) {
+    var legend = document.getElementById('userlegend');
+    var div = document.createElement('div');
+    div.innerHTML = "User: " + name;
+    legend.appendChild(div);    
 }
 
 // Auth with an email/password combination
-trainStationsFirebaseRef.authWithPassword({
-  email    : 'alexis.mp@gmail.com',
-  password : 'foo'
-}, authHandler);
+//trainStationsFirebaseRef.authWithPassword({
+//  email    : 'user@email.com',
+//  password : 'changeme'
+//}, authHandler);
 
-// trainStationsFirebaseRef.authWithOAuthRedirect("google", authHandler);
+trainStationsFirebaseRef.authWithOAuthPopup("google", authHandler);
+//trainStationsFirebaseRef.authWithOAuthRedirect("google", authHandler);
 
+//trainStationsFirebaseRef.onAuth(function(authData) {
+//    console.log(authData);
+//  if (authData !== null) {
+//    console.log("Authenticated successfully with payload:", authData);
+//    console.log("Name: ", authData.google.displayName );
+//  } else {
+//    // Try to authenticate with Google via OAuth redirection
+//    console.log("before auth with redirect...")
+//    trainStationsFirebaseRef.authWithOAuthRedirect("google", function(error, authData) {
+//        console.log("kdjfhsdkjfhksjdfhjkshdfj> ", arguments)
+//      if (error) {
+//        console.log("Login Failed!", error);
+//      }
+//    });
+//  }
+//});
 
 /*************/
 /*  GEOQUERY */
@@ -41,42 +67,42 @@ var stationsInQuery = {};
 
 // Create a new GeoQuery instance
 var geoQuery_stations = geoFire_stations.query({
-  center: center_stations,
-  radius: radiusInKm_stations
-})
+    center: center_stations,
+    radius: radiusInKm_stations
+});
 
 /* Adds new train station markers to the map when they enter the query */
-geoQuery_stations.on("key_entered", function(stationId, stationLocation) {
-  // Specify that the station has entered this query
-  stationsInQuery[stationId] = true;
+geoQuery_stations.on("key_entered", function (stationId, stationLocation) {
+    // Specify that the station has entered this query
+    stationsInQuery[stationId] = true;
 
-  // Look up the train station's data in the appropriate Firebase reference
-  trainStationsFirebaseRef.child(stationId).once("value", function(dataSnapshot) {
-    // Get the train station data from Firebase
-    station = dataSnapshot.val();
+    // Look up the train station's data in the appropriate Firebase reference
+    trainStationsFirebaseRef.child(stationId).once("value", function (dataSnapshot) {
+        // Get the train station data from Firebase
+        station = dataSnapshot.val();
 
-    // If the train station has not already exited this query in the time it
-    // took to look up its data add it to the map
-    if (station !== null && stationsInQuery[stationId] === true) {
-      // Add the train station to the list of stations in the query
-      stationsInQuery[stationId] = station;
-      // Create a new marker for the train station
-      station.marker = createStationMarker(station);
-    }
-  });
+        // If the train station has not already exited this query in the time it
+        // took to look up its data add it to the map
+        if (station !== null && stationsInQuery[stationId] === true) {
+            // Add the train station to the list of stations in the query
+            stationsInQuery[stationId] = station;
+            // Create a new marker for the train station
+            station.marker = createStationMarker(station);
+        }
+    });
 });
 
 /* Removes train station markers from the map when they exit the query */
-geoQuery_stations.on("key_exited", function(stationId, stationLocation) {
-  // Get the train station from the list of stations in the query
-  var station = stationsInQuery[stationId];
+geoQuery_stations.on("key_exited", function (stationId, stationLocation) {
+    // Get the train station from the list of stations in the query
+    var station = stationsInQuery[stationId];
 
-  // If the train station's data has already been loaded from Firebase,
-  // remove its marker from the map
-  if (station !== true) {
-    station.marker.setMap(null);
-  }
+    // If the train station's data has already been loaded from Firebase,
+    // remove its marker from the map
+    if (station !== true) {
+        station.marker.setMap(null);
+    }
 
-  // Remove the train station from the list of stations in the query
-  delete stationsInQuery[stationId];
+    // Remove the train station from the list of stations in the query
+    delete stationsInQuery[stationId];
 });
